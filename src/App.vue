59d7d30194025d12cf14ca77efcb3487d66177db
Filip Krunic <fkrunic9@gmail.com>
2023-09-12 11:01:23 -0700

    mobile form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import CardFront from './components/CardFront.vue';
+import FormInput from './components/FormInput.vue'
 
 onMounted(() => {
   document.body.className = 'font-space-grotesk'
@@ -27,6 +28,60 @@ onMounted(() => {
       </div>      
     </div>
     
-        
+    <!-- Card Form -->  
+    <div class="
+      flex 
+      flex-col 
+      gap-6 
+
+      w-full 
+      p-6 
+      mt-20 
+      
+      text-xs 
+      tracking-widest
+      text-very-dark-violet
+      ">
+
+      <!-- Name -->
+      <div class="flex flex-col gap-1">
+        <p>CARDHOLDER NAME</p>
+        <FormInput :placeholder="'e.g. Jane Appleseed'"></FormInput>
+      </div>
+
+      <!-- Card Number -->
+      <div class="flex flex-col gap-1">
+        <p>CARD NUMBER</p>
+        <FormInput :placeholder="'e.g. 1234 5678 9123 0000'"></FormInput>
+      </div> 
+      
+      <!-- Card Details -->
+      <div class="flex flex-row gap-3">
+
+        <!-- Expiration Date -->
+        <div class="flex flex-col gap-1">
+          <p>EXP. DATE (MM / YY)</p>
+          <div class="flex flex-row gap-2">
+            <FormInput class="w-[4rem]" :placeholder="'MM'"></FormInput>
+            <FormInput class="w-[4rem]" :placeholder="'YY'"></FormInput>
+          </div>
+        </div>
+
+        <!-- Security Code -->
+        <div class="flex flex-col gap-1">
+          <p>CVC</p>
+          <FormInput class="w-[11rem]" :placeholder="'e.g. 123'"></FormInput>
+        </div> 
+      </div>
+
+      <!-- Submit Button -->
+      <p class="
+        p-4 
+        text-center 
+        text-base 
+        rounded-lg 
+        text-white 
+        bg-very-dark-violet">Confirm</p>
+    </div>
   </div>
 </template>
