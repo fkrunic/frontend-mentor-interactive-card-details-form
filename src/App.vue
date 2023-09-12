5e6ebb2db974c5343d61c0a83abfb01fcbf0fd42
Filip Krunic <fkrunic9@gmail.com>
2023-09-11 22:31:01 -0700

    banner construction

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,23 @@ onMounted(() => {
 <template>
 
   <!-- Page Layout -->
-  <div class="relative flex flex-col items-center justify-start min-h-screen">
-    <img src="./assets/bg-main-mobile.png">
+  <div class="flex flex-col items-center justify-start min-h-screen">
 
-    <div class="absolute rounded top-[4%] right-[5%]">
-      <img class="h-[10rem]" src="./assets/bg-card-back.png" alt="card-back">
-      <p class="absolute top-[44%] right-[12%] text-xs tracking-widest text-white/75">000</p>
+    <!-- Banner -->
+    <div class="relative">
+      <img src="./assets/bg-main-mobile.png">
+
+      <!-- Card Back -->
+      <div class="absolute rounded top-[15%] right-[5%]">
+        <img class="h-[10rem]" src="./assets/bg-card-back.png" alt="card-back">
+        <p class="absolute top-[45%] right-[12%] text-[7pt] tracking-widest text-white/75">000</p>
+      </div>      
+
+      <div class="absolute top-[53%] left-[4%]">
+        <CardFront></CardFront>
+      </div>      
     </div>
-
     
-    <CardFront></CardFront>
-    
+        
   </div>
 </template>
