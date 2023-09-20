4fe6a581e2a0aa36c3efa5489581967ca81ca779
Filip Krunic <fkrunic9@gmail.com>
2023-09-20 09:28:11 -0700

    centered cards with negative margin on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import CardFront from './components/CardFront.vue';
-import CardBack from './components/CardBack.vue';
+import Phone from './components/Phone.vue';
 
 onMounted(() => {
   document.body.className = 'font-space-grotesk'
@@ -9,8 +8,5 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen">
-    <CardFront :digits="'0000 0000 0000 0000'" :name="'JANE APPLESEED'" :expiration="'00/00'"></CardFront>
-    <CardBack :cvc="'000'"></CardBack>
-  </div>
+  <Phone></Phone>
 </template>
---
+++ src/components/Phone.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import CardBack from './CardBack.vue';
+import CardFront from './CardFront.vue';
+</script>
+
+<template>
+  <!-- Root -->
+  <div>
+
+    <!-- Phone -->
+    <div class="flex flex-col items-center">
+
+      <!-- Banner Background -->
+      <img class="w-full h-[240px]" src="../assets/bg-main-mobile.png" alt="banner">
+
+      <!-- Content -->
+      <div class="flex flex-col gap-[50px] items-center -mt-[209px]">
+
+        <!-- Cards -->
+        <div class="relative flex flex-col items-center">
+
+          <!-- Card Back Spacer -->
+          <div class="flex flex-col gap-[10px] items-center pl-[55px]">
+            <CardBack :cvc="'000'"></CardBack>
+          </div>
+
+          <CardFront 
+            class="absolute -left-[2px] top-[96px]"
+            :digits="'0000 0000 0000 0000'" 
+            :name="'JANE APPLESEED'" 
+            :expiration="'00/00'"
+            ></CardFront>        
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
