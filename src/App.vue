5a027cf6da18994d97f982f213af0dfbbd262bcf
Filip Krunic <fkrunic9@gmail.com>
2023-09-20 09:01:33 -0700

    card back mobile

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import CardFront from './components/CardFront.vue';
+import CardBack from './components/CardBack.vue';
 
 onMounted(() => {
   document.body.className = 'font-space-grotesk'
@@ -10,5 +11,6 @@ onMounted(() => {
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <CardFront :digits="'0000 0000 0000 0000'" :name="'JANE APPLESEED'" :expiration="'00/00'"></CardFront>
+    <CardBack :cvc="'000'"></CardBack>
   </div>
 </template>
---
+++ src/components/CardBack.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{ cvc: string }>()
+</script>
+
+<template>
+  <!-- Root -->
+  <div>
+    <!-- Card Back -->
+    <div class="relative flex flex-col gap-[10px]">
+
+      <!-- Card Back Background -->
+      <img class="
+        w-[286px] 
+        h-[158px]
+        " 
+        src="../assets/bg-card-back.png" 
+        alt="card-back">
+
+      <!-- Digits -->
+      <p class="
+        absolute 
+        left-[230px] 
+        top-[70px] 
+        font-bold 
+        text-[10px] 
+        tracking-[0.8px] 
+        text-white
+        ">{{ cvc }}</p>
+    </div>
+  </div>
+</template>
