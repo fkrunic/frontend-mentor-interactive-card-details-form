ca7756013142b70f9ba8f297d54a6e6739aae5d8
Filip Krunic <fkrunic9@gmail.com>
2023-09-20 08:55:02 -0700

    card front desktop

--- src/components/CardFront.vue
+++ src/components/CardFront.vue
@@ -3,21 +3,65 @@ defineProps<{ digits: string, name: string, expiration: string }>()
 </script>
 
 <template>
-  <div class="relative flex flex-col gap-[10px]">
+  <!-- Root -->
+  <div>
 
-    <!-- Card Background -->
-    <img class="w-[287px] h-[156px] rounded-[5px]" src="../assets/bg-card-front.png" alt="card-front">
+    <!-- Card Front -->
+    <div class="relative flex flex-col gap-[10px]">
 
-    <!-- Card Logo -->
-    <img class="absolute left-[19px] top-[17px] w-[56px] h-[30px]" src="../assets/card-logo.svg" alt="card-logo">
+      <!-- Card Background -->
+      <img class="
+        w-[287px] desktop:w-[447px]
+        h-[156px] desktop:h-[245px]
+        rounded-[5px]
+        " 
+        src="../assets/bg-card-front.png" 
+        alt="card-front">
 
-    <!-- Card Digits -->
-    <p class="absolute left-[20px] top-[85px] font-bold text-[18px] tracking-[2.16px] text-white">{{ digits }}</p>
+      <!-- Card Logo -->
+      <img class="
+        absolute 
+        left-[19px] desktop:left-[30px]
+        top-[17px] desktop:top-[28px]
+        w-[56px] desktop:w-[88px]
+        h-[30px] desktop:h-[49px]
+        " 
+        src="../assets/card-logo.svg" 
+        alt="card-logo">
 
-    <!-- Name -->
-    <p class="absolute left-[20px] top-[124px] font-bold text-[10px] tracking-[0.8px] text-white">{{ name }}</p>
+      <!-- Card Digits -->
+      <p class="
+        absolute 
+        left-[20px] desktop:left-[33px]
+        top-[85px] desktop:top-[140px]
+        font-bold 
+        text-[18px] desktop:text-[30px]
+        tracking-[2.16px] desktop:tracking-[2.1px]
+        text-white
+        "
+        >{{ digits }}</p>
 
-    <!-- Expiration -->
-    <p class="absolute left-[232px] top-[124px] font-bold text-[10px] tracking-[0.8px] text-white">{{ expiration }}</p>
+      <!-- Name -->
+      <p class="
+        absolute 
+        left-[20px] desktop:left-[33px]
+        top-[124px] desktop:top-[202px]
+        font-bold 
+        text-[10px] desktop:text-[14px]
+        tracking-[0.8px] desktop:tracking-[1.96px]
+        text-white
+        ">{{ name }}</p>
+
+      <!-- Expiration -->
+      <p class="
+        absolute 
+        left-[232px] desktop:left-[365px]
+        top-[124px] desktop:top-[202px]
+        font-bold 
+        text-[10px] desktop:text-[14px]
+        tracking-[0.8px] desktop:tracking-[1.68px]
+        text-white
+        ">{{ expiration }}</p>
+      </div>
   </div>
 </template>
