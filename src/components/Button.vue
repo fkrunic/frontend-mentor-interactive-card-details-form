6e5ed56cb03eda7dcb0f834b75d12d354257f161
Filip Krunic <fkrunic9@gmail.com>
2023-09-21 12:41:52 -0700

    submit button

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type ButtonType = 'Confirm' | 'Continue'
+defineProps<{ buttonType: ButtonType }>()
+const emit = defineEmits<{ (event: 'submit'):void }>()
+const onClick = (): void => emit('submit')
+</script>
+
+<template>
+  <p class="
+    w-[327px] desktop:w-[380px]
+    h-[53px]
+    px-[129px] 
+    py-[15px] 
+    rounded-[10px] 
+    text-center
+    bg-very-dark-violet
+    font-bold
+    text-[18px]
+    text-white
+    hover:cursor-pointer
+    "
+    @click="onClick"
+    >{{ buttonType }}</p>
+</template>
