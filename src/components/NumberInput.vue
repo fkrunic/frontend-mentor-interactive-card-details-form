d6d574fbfdd700e0187d57d541a52e8f3f932dda
Filip Krunic <fkrunic9@gmail.com>
2023-09-21 10:39:38 -0700

    number input

---
+++ src/components/NumberInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FieldBox from './FieldBox.vue';
+import FieldInput from './FieldInput.vue';
+import { useNumberStore } from '../app'
+
+const store = useNumberStore()
+const onInput = (input: string): void => store.update(input)
+</script>
+
+<template>
+  <FieldBox :title="'CARD NUMBER'" :status="store.status">
+    <FieldInput 
+      :placeholder="'e.g. 1234 5678 9123 0000'" 
+      :is-error="store.status.kind === 'error'"
+      :length="'long'"
+      @input="onInput"
+      ></FieldInput>
+  </FieldBox>
+</template>
