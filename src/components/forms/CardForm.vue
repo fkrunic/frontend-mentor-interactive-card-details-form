e4589bcd0fedc66493b6fcf4821df4cfbd12e0ee
Filip Krunic <fkrunic9@gmail.com>
2023-09-21 15:35:01 -0700

    card form

---
+++ src/components/forms/CardForm.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import NameInput from '@/components/inputs/NameInput.vue'
+import NumberInput from '@/components/inputs/NumberInput.vue'
+import ExpirationInput from '@/components/inputs/ExpirationInput.vue'
+import CVCInput from '@/components/inputs/CVCInput.vue'
+import ConfirmButton from '@/components/buttons/ConfirmButton.vue'
+</script>
+
+<template>
+  <!-- Root -->
+  <div>
+    <div class="flex flex-col gap-[30px] items-center">
+      <!-- Card Inputs -->
+      <div class="flex flex-col gap-[50px] pb-[30px]">
+        <NameInput></NameInput>
+        <NumberInput></NumberInput>
+        <!-- Bottom Inputs -->
+        <div class="flex flex-row gap-[11px] desktop:gap-[21px]">
+          <ExpirationInput></ExpirationInput>
+          <CVCInput></CVCInput>
+        </div>        
+      </div>
+      <ConfirmButton></ConfirmButton>
+    </div>
+  </div>
+</template>
