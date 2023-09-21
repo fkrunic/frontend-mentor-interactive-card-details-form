fd80b8a361e8136ceb0982ea40aaaaf691189721
Filip Krunic <fkrunic9@gmail.com>
2023-09-21 10:29:57 -0700

    field box component

---
+++ src/components/FieldBox.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import FieldTitle from './FieldTitle.vue';
+import FieldError from './FieldError.vue';
+import { Status } from '../app';
+defineProps<{ title: string, status: Status }>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-[9px]">
+    <FieldTitle :title="title"></FieldTitle>
+    <slot></slot>
+    <FieldError v-if="status.kind === 'error'" :message="status.message"></FieldError>
+  </div>  
+</template>
