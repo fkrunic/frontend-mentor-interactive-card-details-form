2bdc647a32fe1255e69c0625805c59beb7df5816
Filip Krunic <fkrunic9@gmail.com>
2023-09-21 13:33:27 -0700

    cvc input

---
+++ src/components/inputs/CVCInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FieldBox from '../fields/FieldBox.vue';
+import FieldInput from '../fields/FieldInput.vue';
+import { useCVCStore } from '@/app';
+
+const store = useCVCStore()
+const onInput = (input: string): void => store.updateInput(input)
+</script>
+
+<template>
+  <FieldBox :title="'CVC'" :status="store.status">
+    <FieldInput
+      :placeholder="'e.g. 123'"
+      :is-error="store.status.kind === 'error'"
+      :length="'medium'"
+      @input="onInput"
+      ></FieldInput>
+  </FieldBox>
+</template>
